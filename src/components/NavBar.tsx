8cb87ca0cd16e3d8b094b14a139e84f477919454
Frank <fcojavierlpz91@gmail.com>
2023-11-28 20:29:08 -0600

    refactor: update logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,18 @@
 import { HStack, Image } from '@chakra-ui/react'
-import logo from '../assets/logo.webp'
+import logo from '../assets/logo.jpeg'
 import ColorModeSwitch from './ColorModeSwitch'
 
 const NavBar = () => {
   return (
     <HStack justifyContent="space-between" padding="10px">
-      <Image src={logo} boxSize="100px" margin={5} />
+      <Image
+        src={logo}
+        boxSize="100px"
+        margin={5}
+        borderRadius="full"
+        transition="transform 0.2s ease-in-out"
+        _hover={{ transform: 'scale(1.1)' }}
+      />
       <ColorModeSwitch />
     </HStack>
   )
